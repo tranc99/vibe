240a1803050635f9f3a9e43249c875336667c0ba
Ten Mutunhire <tmutunhire@gmail.com>
2015-04-29 09:22:16 +0000

    make a video playing app

---
+++ app/src/main/java/com/vibes/vibe/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.vibes.vibe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, ListActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 5000);
+    }
+
+}
