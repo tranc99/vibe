ddc3cbe1b51cb6dd2ab28b3528227bc30e2f5f59
Ten Mutunhire <tmutunhire@gmail.com>
2015-05-03 21:53:14 +0000

    handle history

--- app/src/main/java/com/vibes/vibe/ListActivity.java
+++ app/src/main/java/com/vibes/vibe/ListActivity.java
@@ -2,6 +2,7 @@ package com.vibes.vibe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -23,6 +24,29 @@ public class ListActivity extends Activity {
         engine.loadUrl("http://youtube.com/");
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && engine.canGoBack()) {
+            engine.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_FORWARD) && engine.canGoForward()) {
+            engine.goForward();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyUp(keyCode, event);
+
+    }
+
     private class MyWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -30,4 +54,6 @@ public class ListActivity extends Activity {
             return true;
         }
     }
+
+
 }
