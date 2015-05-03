1ac9d93a5f9eb6fa40b06905b110ebc9b25221b8
Ten Mutunhire <tmutunhire@gmail.com>
2015-05-03 21:33:27 +0000

    persistent videos

--- app/src/main/java/com/vibes/vibe/ListActivity.java
+++ app/src/main/java/com/vibes/vibe/ListActivity.java
@@ -1,104 +1,33 @@
 package com.vibes.vibe;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import com.squareup.picasso.Picasso;
-
-import java.util.List;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class ListActivity extends Activity {
 
-    private EditText searchInput;
-    private ListView videosFound;
-
-    private Handler handler;
-    private List<VideoItem> searchResults;
+    private WebView engine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        searchInput = (EditText) findViewById(R.id.search_text);
-        videosFound = (ListView) findViewById(R.id.videos);
+        engine = (WebView) findViewById(R.id.web_engine);
+        engine.setWebViewClient(new MyWebViewClient());
 
-        handler = new Handler();
+        engine.getSettings().setJavaScriptEnabled(true);
 
-        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if(actionId == EditorInfo.IME_ACTION_DONE) {
-                    searchOnYoutube(v.getText().toString());
-                    return false;
-                }
-                return true;
-            }
-        });
+        engine.loadUrl("http://youtube.com/");
     }
 
-
-    private void searchOnYoutube(final String keywords) {
-        new Thread() {
-            public void run() {
-                GetYoutube butler = new GetYoutube(ListActivity.this);
-                searchResults = butler.search(keywords);
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        updateVideos();
-                    }
-                });
-            }
-        }.start();
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
-
-    private void updateVideos() {
-        ArrayAdapter<VideoItem> adapter = new ArrayAdapter<VideoItem>(getApplicationContext(), R.layout.each_video, searchResults) {
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                if (convertView == null) {
-                    convertView = getLayoutInflater().inflate(R.layout.each_video, parent, false);
-                }
-                ImageView thumbnail = (ImageView) convertView.findViewById(R.id.thumbnail);
-                TextView title = (TextView) convertView.findViewById(R.id.video_title);
-                TextView description = (TextView) convertView.findViewById(R.id.description);
-
-                VideoItem searchResult = searchResults.get(position);
-
-                Picasso.with(getApplicationContext()).load(searchResult.thumbnailUrl).into(thumbnail);
-                title.setText(searchResult.title);
-                description.setText(searchResult.description);
-                return convertView;
-            }
-        };
-
-        videosFound.setAdapter(adapter);
-    }
-
-    private void addClickListener() {
-        videosFound.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), PlayerActivity.class);
-                intent.putExtra("VIDEO_ID", searchResults.get(position).id);
-                startActivity(intent);
-            }
-        });
-    }
-
 }
